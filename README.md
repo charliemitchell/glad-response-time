cd620c47002c1746aa69f55e6c3daefc59083350
charles mitchell <charliesemailis@gmail.com>
2016-03-24 10:18:22 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+## Glad Response Time
+A plugin for the Glad JS Framework to track response times. This is a "Internal" approach as it does not communicate with any other API's.
+
+## To Use
+`npm i glad-response-times` in your project's src directory. 
+This will create a collection named `glad_response_times` and store your response time there.
+Be advised that unless you choose the all option, you won't see any logs until you reach the nth request as specified by the `logEvery` 
+parameter in your config, or the 5th by default. Currently, It will be up to you to implement a User interface to view your response times.
+
+## Options
+In your `config.js` file *(the one in your project's src directory)* you can add an entry for gladResponseTimes. The default configuration logs the response time of every 5th request.
+You should really think about what this number should be given your traffic because this collection will grow pretty quick.
+
+Example of configuring the plugin to log the response time every 10th request
+
+```
+    { 
+        
+    
+        gladResponseTimes : {
+          logEvery : 10
+        }
+        
+        //...
+    }
+```
+
+Example of configuring the plugin to log the response time every request
+
+```
+    { 
+    
+        gladResponseTimes : {
+          all : true
+        }
+        
+        //...
+    }
+```
+
+Example of configuring the plugin to log the response time every other request
+
+```
+    { 
+
+        gladResponseTimes : {
+          logEvery : 2
+        }
+        
+        //...
+    }
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+var glad = require('glad');
+var config = require(process.cwd() + '/config');
+
+module.exports = {
+
+    name : 'Glad Response Time',
+
+    minGladVersion : '0.5.6',
+
+    hook : 'onBeforeBodyParser',
+
+    method : function (server, app, express, next) {
+
+        var mongoose = require('glad').mongoose;
+        var tokenizer = require('glad').tokenizer;
+        var moment = require('glad').moment;
+        var Schema = mongoose.Schema;
+
+        var responseTimes = new Schema({
+            url : {
+                type : String
+            },
+            method : {
+                type : String
+            },
+            responseTime : {
+                type : Number
+            },
+            date: {
+                type : Date
+            }
+        });
+
+        var model = mongoose.model('glad_response_time', responseTimes);
+        var count = 0;
+
+        app.use(function (req, res, callback) {
+
+
+            var skip = 5;
+
+            if (config.gladResponseTimes && config.gladResponseTimes.all) {
+                skip = 1;
+            } else if (config.gladResponseTimes && config.gladResponseTimes.logEvery) {
+                skip = config.gladResponseTimes.logEvery || 1;
+            }
+
+            req.on('end', function () {
+                count += 1;
+
+                if (count % skip === 0) {
+                    count = 0;
+                    var now = new Date().getTime();
+                    var start = tokenizer.timeDecoded(req.id);
+
+                    new model({
+                        url : req.url,
+                        method: req.method,
+                        responseTime : now - start,
+                        date : new moment(start).toDate()
+                    }).save(function (err) {
+                        if (err) {
+                            console.error('Error saving Response Time From glad-response-times', err);
+                        }
+                    });
+                }
+            });
+            callback();
+        });
+        next();
+    }
+};
